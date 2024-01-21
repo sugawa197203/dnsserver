d3b04bcacf7e099361f88a21325d541b0b135073
sugawa197203 <96975428+sugawa197203@users.noreply.github.com>
2024-01-22 00:51:03 +0900

    c++

--- client.cpp
+++ client.cpp
@@ -5,30 +5,51 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <iostream>
 
 int main()
 {
-	// UDP 通信で送る
-
-	int port = 53;
+	// UDP recive
 	int sock;
 	struct sockaddr_in addr;
+	socklen_t addr_len = sizeof(struct sockaddr_in);
+	char buf[1024];
 
-	sock = socket(AF_INET, SOCK_DGRAM, 0);
+	if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+	{
+		perror("socket");
+		return 1;
+	}
 
+	bzero((char *)&addr, sizeof(addr));
 	addr.sin_family = AF_INET;
-	addr.sin_port = htons(port);
-	addr.sin_addr.s_addr = inet_addr("192.168.213.109");
+	addr.sin_port = htons(25565);
+	addr.sin_addr.s_addr = INADDR_ANY;
 
-	char buf[2048];
+	if (bind(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+	{
+		perror("bind");
+		return 1;
+	}
 
 	memset(buf, 0, sizeof(buf));
 
-	sprintf(buf, "Hello, world!");
+	if (recvfrom(sock, buf, sizeof(buf) - 1, 0, (struct sockaddr *)&addr, &addr_len) < 0)
+	{
+		perror("recvfrom");
+		return 1;
+	}
 
-	sendto(sock, buf, sizeof(buf), 0, (struct sockaddr *)&addr, sizeof(addr));
+	// print hex
 
-	close(sock);
+	for (int i = 0; i < 1024; i++)
+	{
+		printf("%02x ", buf[i]);
+		if (i % 16 == 15)
+		{
+			printf("\n");
+		}
+	}
 
 	return 0;
 }
